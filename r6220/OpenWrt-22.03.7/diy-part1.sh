3a73e97409706ad23887070c79dc8302dce85e73
Xghng999 <1369492@qq.com>
2026-06-25 13:19:54 +0800

    Update diy-part1.sh

--- r6220/OpenWrt-22.03.7/diy-part1.sh
+++ r6220/OpenWrt-22.03.7/diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #wget https://raw.githubusercontent.com/Bartixxx32/r6220-build/main/patchres.diff -O ./target/linux/ramips/patches-5.10/901-staging-mt7621-pci-delay-for-properly-detect.patch
