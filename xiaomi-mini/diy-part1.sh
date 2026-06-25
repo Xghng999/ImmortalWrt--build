a33bf7932ad84fb823c79613788f93d078bbda11
Xghng999 <1369492@qq.com>
2026-06-25 17:10:23 +0800

    Update diy-part1.sh

--- xiaomi-mini/diy-part1.sh
+++ xiaomi-mini/diy-part1.sh
@@ -13,7 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#wget https://raw.githubusercontent.com/Bartixxx32/r6220-build/main/patchres.diff -O ./target/linux/ramips/patches-5.10/901-staging-mt7621-pci-delay-for-properly-detect.patch
+# 3. (可选) 添加其他常用的依赖源，确保编译不报错
+# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
